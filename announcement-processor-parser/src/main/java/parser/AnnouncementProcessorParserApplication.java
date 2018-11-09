d0e328255ab885c687d40f6a8a42c462e7c1dced
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2018-11-09 11:50:39 +0100

    Added scheduler to run application continuously. #18

--- announcement-processor-parser/src/main/java/parser/AnnouncementProcessorParserApplication.java
+++ announcement-processor-parser/src/main/java/parser/AnnouncementProcessorParserApplication.java
@@ -4,9 +4,11 @@ import lombok.extern.java.Log;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Log
 @SpringBootApplication
+@EnableScheduling
 public class AnnouncementProcessorParserApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(AnnouncementProcessorParserApplication.class);
