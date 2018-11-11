91fd4084867812a6309b44f846b7aaa89f5a293f
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2018-11-11 17:51:42 +0100

    Added example test to verify if jacoco works.

---
+++ announcement-processor-extractor/src/test/java/parser/ExampleTest.java
@@ -0,0 +1,14 @@
+package parser;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class ExampleTest {
+    @Test
+    public void test(){
+        Assert.assertTrue(true);
+    }
+}
