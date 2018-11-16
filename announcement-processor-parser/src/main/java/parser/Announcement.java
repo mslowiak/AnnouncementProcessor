075801f99a1c0b76fa52d56fdbb7c0f11e57a9e0
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2018-11-16 22:54:56 +0100

    Added class containing announcement properties. #31

---
+++ announcement-processor-parser/src/main/java/parser/Announcement.java
@@ -0,0 +1,26 @@
+package parser;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Announcement {
+    private String title;
+    private BigDecimal price;
+    private LocalDateTime creationDate;
+    private String lessor;
+    private String propertyType;
+    private Double flatArea;
+    private Integer roomAmount;
+    private Integer bathAmount;
+    private Boolean isParkingAvailable;
+    private Boolean isSmokingAllowed;
+    private Boolean isPetFriendly;
+    private String lessorName;
+    private BigDecimal additionalRentCost;
+    private String level;
+    private String isFurnished;
+    private String description;
+    private String provider;
+    private String url;
+    private String phoneNumber;
+}
