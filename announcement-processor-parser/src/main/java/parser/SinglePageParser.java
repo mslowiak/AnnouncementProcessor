faa34e0a399c6453634379548a3c161fa50c9306
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2018-11-16 22:52:39 +0100

    Interface to implement by concrete parsers. #31

---
+++ announcement-processor-parser/src/main/java/parser/SinglePageParser.java
@@ -0,0 +1,28 @@
+package parser;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public interface SinglePageParser {
+    String parseTitle();
+    BigDecimal parsePrice();
+    LocalDateTime parseCreationDate();
+    String parseDescription();
+    String parseProvider();
+    String parseUrl();
+
+    String parseLessor();
+    String parserLessorName();
+    String parsePhoneNumber();
+
+    String parsePropertyType();
+    Double parseFlatArea();
+    Integer parseRoomAmount();
+    Integer parseBathAmount();
+    Boolean parseParking();
+    Boolean parserSmokers();
+    Boolean parserPetFriendly();
+    BigDecimal parseAdditionalRentCost();
+    String parseLevel();
+    String parseFurnishings();
+}
