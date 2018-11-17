3ed210c657bad456f46402bdd92d743490329829
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2018-11-17 16:36:19 +0100

    Test util function to get file as JSoup Document type. #3

--- announcement-processor-parser/src/test/java/parser/ReaderUtil.java
+++ announcement-processor-parser/src/test/java/parser/ReaderUtil.java
@@ -1,6 +1,10 @@
 package parser;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -8,7 +12,19 @@ import java.util.stream.Stream;
 
 public class ReaderUtil {
 
-    public static String readAllLinesFromFile(String filePath) {
+    public static Document getDocumentToTest(String resourceName) {
+        Document spyDoc = null;
+        try {
+            String path = Paths.get(ReaderUtil.class.getResource(resourceName).toURI()).toString();
+            String fileContent = ReaderUtil.readAllLinesFromFile(path);
+            spyDoc = Jsoup.parse(fileContent);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return spyDoc;
+    }
+
+    private static String readAllLinesFromFile(String filePath) {
         StringBuilder contentBuilder = new StringBuilder();
         try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
             stream.forEach(s -> contentBuilder.append(s).append("\n"));
