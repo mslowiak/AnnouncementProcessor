9f5185e57f1384aace5e4fc2b1b0f78f5902d74c
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2018-11-18 19:57:50 +0100

    Additional tests for properties options. #3

---
+++ announcement-processor-parser/src/test/java/parser/scrapper/options/GumtreeSmokerOptionsTests.java
@@ -0,0 +1,45 @@
+package parser.scrapper.options;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import parser.ReaderUtil;
+import parser.scrapper.GumtreeAnnouncementParser;
+
+public class GumtreeSmokerOptionsTests {
+
+    private GumtreeAnnouncementParser gumtreeParser;
+
+    @Before
+    public void setUp() {
+        gumtreeParser = new GumtreeAnnouncementParser();
+    }
+
+    @Test
+    public void testSmokersYes(){
+        // given
+        Document spyDoc = ReaderUtil.getDocumentToTest("/gumtree/options/smoker/yes.html");
+        Element detailsElement = spyDoc.selectFirst(".vip-details");
+
+        // when
+        Boolean smokers = gumtreeParser.parserSmokers(detailsElement);
+
+        // then
+        Assert.assertTrue(smokers);
+    }
+
+    @Test
+    public void testSmokersNo(){
+        // given
+        Document spyDoc = ReaderUtil.getDocumentToTest("/gumtree/options/smoker/no.html");
+        Element detailsElement = spyDoc.selectFirst(".vip-details");
+
+        // when
+        Boolean smokers = gumtreeParser.parserSmokers(detailsElement);
+
+        // then
+        Assert.assertFalse(smokers);
+    }
+}
