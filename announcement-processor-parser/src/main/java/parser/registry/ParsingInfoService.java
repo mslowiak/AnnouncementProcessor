925bcf0dc8070e79c5aa8f4dc1571dec3553ceec
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2018-12-01 23:38:33 +0100

    Implemented logic of fetching last record from db. #10

--- announcement-processor-parser/src/main/java/parser/registry/ParsingInfoService.java
+++ announcement-processor-parser/src/main/java/parser/registry/ParsingInfoService.java
@@ -2,6 +2,8 @@ package parser.registry;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ParsingInfoService {
 
@@ -9,5 +11,25 @@ public class ParsingInfoService {
 
     public ParsingInfoService(ParsingInfoRepository parsingInfoRepository) {
         this.parsingInfoRepository = parsingInfoRepository;
+    }
+
+    public Optional<ParsingInfo> fetchLastRecordForProvider(String provider, ParsingInfo lastParsedAnnouncement) {
+        Optional<ParsingInfo> announcement;
+        if (lastParsedAnnouncement == null) {
+            announcement = parsingInfoRepository.findFirstByProviderOrderByDateDescCounterPerDateDesc(provider);
+        } else {
+            announcement = parsingInfoRepository.findFirstByProviderAndDateEqualsAndCounterPerDateLessThanOrderByCounterPerDateDesc(
+                    provider,
+                    lastParsedAnnouncement.getDate(),
+                    lastParsedAnnouncement.getCounterPerDate()
+            );
+            if (!announcement.isPresent()) {
+                announcement = parsingInfoRepository.findFirstByProviderAndDateLessThanOrderByDateDescCounterPerDateDesc(
+                        provider,
+                        lastParsedAnnouncement.getDate()
+                );
+            }
+        }
+        return announcement;
     }
 }
