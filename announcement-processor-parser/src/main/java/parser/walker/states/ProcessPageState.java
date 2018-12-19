53d0b52fcf99efe699c14e3d78db3ba9ae0283d7
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2018-12-19 17:48:51 +0100

    Allow to parse announcements on page via multiple threads. #10

--- announcement-processor-parser/src/main/java/parser/walker/states/ProcessPageState.java
+++ announcement-processor-parser/src/main/java/parser/walker/states/ProcessPageState.java
@@ -11,12 +11,16 @@ import parser.walker.helpers.ProviderHelper;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class ProcessPageState extends WalkerState {
     private HashMap<String, Object> pageWithAnnouncement;
+    private final int THREAD_POOL_SIZE = 10;
 
     public ProcessPageState(ProviderHelper providerHelper, AnnouncementParser announcementParser, HashMap<String, Object> pageWithAnnouncement) {
         super(providerHelper, announcementParser);
@@ -27,13 +31,23 @@ public class ProcessPageState extends WalkerState {
     public WalkerState run() {
         Document pageDocument = (Document) pageWithAnnouncement.get("pageDocument");
         int divNumber = (int) pageWithAnnouncement.get("divNumber");
-
         List<String> urlsToParse = providerHelper.getUrlsToParse(pageDocument, divNumber);
-        for (String urlToParse : urlsToParse) {
-            Announcement announcement = announcementParser.parsePage(urlToParse);
-            // TODO: Save record to DB_PARSER with valid counter
-        }
 
+        log.info("Number of pages to parse: " + urlsToParse.size());
+
+        try {
+            ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+            for (String urlToParse : urlsToParse) {
+                executorService.submit(() -> {
+                    Announcement announcement = announcementParser.parsePage(urlToParse);
+                });
+                // TODO: Save record to DB_PARSER with valid counter
+            }
+            executorService.shutdown();
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            log.error("Interrupted exception");
+        }
         log.info("Parsing urls from page " + providerHelper.getActualPageURLNumber() + " done. Go to VisibleAfterReloadState");
 
         return new VisibleAfterReloadState(providerHelper);
