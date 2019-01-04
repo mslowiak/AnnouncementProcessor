b99851848daf8e04a54cdca221036c9bc427526a
michalsroka <rac.michal.s@gmail.com>
2019-01-05 00:24:07 +0100

    Add operations to the scheduled process #36

--- announcement-processor-extractor/src/main/java/extractor/consumer/ReceivementScheduler.java
+++ announcement-processor-extractor/src/main/java/extractor/consumer/ReceivementScheduler.java
@@ -1,5 +1,8 @@
 package extractor.consumer;
 
+import extractor.dto.AnnouncementDto;
+import extractor.entity.Announcement;
+import extractor.service.AnnouncementExtractingService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,9 +13,20 @@ import org.springframework.stereotype.Service;
 @EnableScheduling
 public class ReceivementScheduler {
 
+    private AnnouncementConsumerJms consumer;
+    private AnnouncementExtractingService extractor;
+
+    public ReceivementScheduler() {
+        this.consumer = new AnnouncementConsumerJms();
+        this.extractor = new AnnouncementExtractingService();
+    }
+
     @Scheduled(cron= "0 */2 * * * *")
     private void scheduleTask() {
+
         log.info("Executing ReceivementScheduler scheduledTask");
-        
+        AnnouncementDto announcementDto = consumer.consumeAnnouncement();
+        Announcement announcement = extractor.extractFromAnnouncementDto(announcementDto);
+        // TODO save to the DB
     }
 }
