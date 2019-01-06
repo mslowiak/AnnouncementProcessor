82d5f0f0fb45113a18cc78bff959f7fefdbc3241
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2019-01-06 18:17:19 +0100

    Added @Data annotation

--- announcement-processor-api/src/main/java/api/entity/price/ContactPrice.java
+++ announcement-processor-api/src/main/java/api/entity/price/ContactPrice.java
@@ -1,11 +1,13 @@
 package api.entity.price;
 
 import api.entity.PriceOffer;
+import lombok.Data;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@Data
 @DiscriminatorValue(value = "contact")
 public class ContactPrice extends PriceOffer {
     public ContactPrice() {
