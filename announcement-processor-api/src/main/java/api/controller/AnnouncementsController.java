00e51ba34ee19de614775d68b9c42ef609572389
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2019-01-06 18:33:39 +0100

    Controller update mappings

--- announcement-processor-api/src/main/java/api/controller/AnnouncementsController.java
+++ announcement-processor-api/src/main/java/api/controller/AnnouncementsController.java
@@ -1,9 +1,14 @@
 package api.controller;
 
+import api.model.DetailedAnnouncementInfo;
+import api.model.GeneralAnnouncementInfo;
 import api.service.ExtractorDataService;
 import api.service.FrontEndDataService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/announcements")
@@ -14,5 +19,35 @@ public class AnnouncementsController {
     public AnnouncementsController(ExtractorDataService extractorDataService, FrontEndDataService frontEndDataService) {
         this.extractorDataService = extractorDataService;
         this.frontEndDataService = frontEndDataService;
+    }
+
+    @GetMapping("/get/all")
+    public ResponseEntity<List<GeneralAnnouncementInfo>> getAllAnnouncements(@RequestParam String currency) {
+        List<GeneralAnnouncementInfo> allAnnouncements = frontEndDataService.getAllAnnouncements(currency);
+        if (allAnnouncements != null) {
+            return new ResponseEntity<>(allAnnouncements, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("get/{id}/detailed-info")
+    public ResponseEntity<DetailedAnnouncementInfo> getDetailedInfoAnnouncement(@PathVariable Integer id, @RequestParam String currency) {
+        DetailedAnnouncementInfo detailedInfoAnnouncement = frontEndDataService.getDetailedInfoAnnouncement(id, currency);
+        if (detailedInfoAnnouncement != null) {
+            return new ResponseEntity<>(detailedInfoAnnouncement, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("get/{id}/general-info")
+    public ResponseEntity<GeneralAnnouncementInfo> getGeneralInfoAnnouncement(@PathVariable Integer id, @RequestParam String currency) {
+        GeneralAnnouncementInfo generalInfoAnnouncement = frontEndDataService.getGeneralInfoAnnouncement(id, currency);
+        if (generalInfoAnnouncement != null) {
+            return new ResponseEntity<>(generalInfoAnnouncement, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
