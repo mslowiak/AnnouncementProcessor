ea48a162945517cd96be163a05ea40a6aff8a50b
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2019-04-14 19:20:49 +0200

    Add Navbar template

--- krak-rent-frontend/src/App.js
+++ krak-rent-frontend/src/App.js
@@ -4,28 +4,27 @@ import {
   Route
 } from 'react-router-dom'
 
-import Navbar from './components/Navbar'
+import MyNavbar from "./components/MyNavbar";
 import AddAnnouncementPage from "./pages/AddAnnouncementPage"
 import AnnouncementInfoPage from "./pages/AnnouncementInfoPage"
 import HomePage from "./pages/HomePage"
 import SearchPage from "./pages/SearchPage"
 import SearchResultsPage from "./pages/SearchResultsPage"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar/>
-        <Router>
-          <div className="container">
-            <Route exact path="/" component={HomePage} />
-            <Route path="/add" component={AddAnnouncementPage} />
-            <Route path="/offer/:id" component={AnnouncementInfoPage} />
-            <Route path="/search" component={SearchPage} />
-            <Route path="/announcements" component={SearchResultsPage} />
-          </div>
-        </Router>
-      </div>
+      <Router>
+        <MyNavbar/>
+        <div className="container" style={{backgroundColor: "lightgray", height: "100vh"}}>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/add" component={AddAnnouncementPage} />
+          <Route path="/offer/:id" component={AnnouncementInfoPage} />
+          <Route path="/search" component={SearchPage} />
+          <Route path="/announcements" component={SearchResultsPage} />
+        </div>
+      </Router>
     )
   }
 }
