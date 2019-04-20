fcb7ca93cd4c3755bf93be6cd8fe1c6b6c9624b1
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2019-04-20 14:30:07 +0200

    Add general announcements container with fetched data from API

--- krak-rent-frontend/src/components/GeneralAnnouncement.js
+++ krak-rent-frontend/src/components/GeneralAnnouncement.js
@@ -1,26 +1,61 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import '../css/general-announcement.css'
 
 class GeneralAnnouncement extends Component {
+    cutString(text, cutLength) {
+        if (text.length > cutLength) {
+            return text.substring(0, cutLength) + '...';
+        } else {
+            return text;
+        }
+    }
+
+    convertDateTime(dateTime) {
+        var currentDate = moment();
+        var offSet = currentDate.utcOffset();
+        var comparedDateTime = moment(dateTime).utcOffset(offSet);
+
+        var isSameDay = moment(currentDate).isSame(comparedDateTime, 'day');
+        if (isSameDay) {
+            var hours = currentDate.diff(comparedDateTime, 'hours', true);
+            if (hours < 1) {
+                var minutes = currentDate.diff(comparedDateTime, 'minutes', true);
+                return Math.round(minutes) + " min temu";
+            }
+            return Math.round(hours) + " godz temu";
+        } else {
+            return comparedDateTime.format('DD-MM-YYYY');
+        }
+    }
+
+    titleLength = 50
+    descriptionLength = 100
+
     render() {
+        const item = this.props.item;
+        const localization = item.localization ? item.localization : "";
+        const dateTime = this.convertDateTime(item.creationDate)
+        const title = this.cutString(item.title, this.titleLength);
+        const description = this.cutString(item.description, this.descriptionLength);
         return (
             <div className="frame">
                 <div className="image-container">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Temp_plate_blue.svg/601px-Temp_plate_blue.svg.png" alt=""/>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Temp_plate_blue.svg/601px-Temp_plate_blue.svg.png" alt="" />
                 </div>
                 <div className="info-container">
                     <div className="details">
-                        <div className="title">Title</div>
-                        <div className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rhoncus, erat ac dictum bibendum.</div>
+                        <div className="title">{title}</div>
+                        <div className="description">{description}</div>
                         <div className="bottom">
-                            <div className="localization">Stare Miasto, Kraków</div>
-                            <div className="creation-date">dzisiaj 16:00</div>
+                            <div className="localization">{localization}</div>
+                            <div className="creation-date">dodano: {dateTime}</div>
                         </div>
                     </div>
                     <div className="pricing">
-                        <div className="price">2200 zł</div>
-                        <div>Gumtree</div>
-                        <div>Właściciel</div>
+                        <div className="price">{item.baseCost} zł</div>
+                        <div>{item.provider}</div>
+                        <div>{item.lessorType}</div>
                     </div>
                 </div>
             </div>
