91e9a7669a81d86ebca739271892f9b762257839
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2019-04-20 13:02:13 +0200

    Add container for all announcements and styling adjustment

--- krak-rent-frontend/src/pages/HomePage.js
+++ krak-rent-frontend/src/pages/HomePage.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+import GeneralAnnouncementContainer from "../components/GeneralAnnouncementContainer";
 
 class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      announcements: []
+    };
+  }
+
+  componentDidMount() {
+    Axios.get('announcements/get/all')
+      .then(
+        results => {
+          this.setState({
+            announcements: results.data
+          })
+        }
+      )
+  }
+
   render() {
+    const dataComponent = this.state.announcements && this.state.announcements.length ? <GeneralAnnouncementContainer data={this.state.announcements} /> : <div>Loading...</div>
     return (
-      <div>HomePage page</div>
+      <div>
+        <div>HomePage page</div>
+        {dataComponent}
+      </div>
     );
   }
 }
