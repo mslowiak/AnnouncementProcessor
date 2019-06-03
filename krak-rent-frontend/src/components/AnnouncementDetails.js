ffdb4b0dfa0988414b13f5e1e520e0972328e36f
Michal Sroka <michal.sroka@comarch.com>
2019-06-03 18:44:41 +0200

    Add lessor to announcement details

--- krak-rent-frontend/src/components/AnnouncementDetails.js
+++ krak-rent-frontend/src/components/AnnouncementDetails.js
@@ -1,15 +1,47 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import translation from "../pl-translation.json";
 
 const AnnouncementDetails = props => {
-  console.log(props.announcement);
+  const getLessorDetails = () => {
+    let lessorDetails = [];
+    // TODO: change to more elegant solution
+    console.log(props.announcement.lessor);
+    const lessor = props.announcement.lessor;
+    for (var key in lessor) {
+      if (lessor.hasOwnProperty(key)) {
+        if (lessor[key] != null && key != "id") {
+          lessorDetails.push(
+            <Col>
+              {translation[key]}: {lessor[key]}
+            </Col>
+          );
+        }
+      }
+    }
+    // for (const [key, value] of props.announcement.lessor.entries()) {
+    //   if (value != null) {
+    //     lessorDetails.push(
+    //       <Col>
+    //         {translation[key]}: {value}
+    //       </Col>
+    //     );
+    //   }
+    // }
+    return (
+      <Container>
+        <Row>{translation["lessor"]}</Row>
+        <Row>{lessorDetails}</Row>
+      </Container>
+    );
+  };
+
+  const isDataLoaded = Object.keys(props.announcement).length > 0;
+  const lessorDetails = isDataLoaded ? getLessorDetails() : [];
 
   return (
     <Container>
-      <Row>
-        <Col>1 of 2</Col>
-        <Col>2 of 2</Col>
-      </Row>
+      {lessorDetails}
       <Row>
         <Col>1 of 3</Col>
         <Col>2 of 3</Col>
