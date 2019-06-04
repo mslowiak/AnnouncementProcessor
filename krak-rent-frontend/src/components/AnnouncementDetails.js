c0e0796625e819fadaf759adea07ad2d2401abb5
michalsroka <rac.michal.s@gmail.com>
2019-06-04 20:47:09 +0200

    Add details info to announcement info page

--- krak-rent-frontend/src/components/AnnouncementDetails.js
+++ krak-rent-frontend/src/components/AnnouncementDetails.js
@@ -1,52 +1,120 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import translation from "../pl-translation.json";
+import "../css/announcement-details.css";
 
 const AnnouncementDetails = props => {
-  const getLessorDetails = () => {
-    let lessorDetails = [];
+  // TODO: no translation available
+  // TODO: there are elements in props such as key:value, key:dict_value, key:dict_of_dicts... building a tree based component automatically would be nice
+  const getFieldDetails = (targetKey, detailsTarget) => {
+    let details = [];
+    let containsData = false;
     // TODO: change to more elegant solution
-    console.log(props.announcement.lessor);
-    const lessor = props.announcement.lessor;
-    for (var key in lessor) {
-      if (lessor.hasOwnProperty(key)) {
-        if (lessor[key] != null && key != "id") {
-          lessorDetails.push(
-            <Col>
-              {translation[key]}: {lessor[key]}
-            </Col>
+    for (var key in detailsTarget) {
+      if (detailsTarget.hasOwnProperty(key)) {
+        if (detailsTarget[key] !== null && key !== "id") {
+          containsData = true;
+          details.push(
+            <span className="details-column">
+              <b>{translation[key]}: </b>
+              {typeof detailsTarget[key] !== "boolean"
+                ? detailsTarget[key]
+                : detailsTarget[key]
+                ? "tak"
+                : "nie"}
+            </span>
           );
         }
       }
     }
-    // for (const [key, value] of props.announcement.lessor.entries()) {
-    //   if (value != null) {
-    //     lessorDetails.push(
-    //       <Col>
-    //         {translation[key]}: {value}
-    //       </Col>
-    //     );
-    //   }
-    // }
+    return containsData ? (
+      <Container className="details-container">
+        <Row>
+          <h5>{translation[targetKey]}</h5>
+        </Row>
+        <Row>{details}</Row>
+      </Container>
+    ) : (
+      []
+    );
+  };
+
+  const getDetails = () => {
+    let details = [];
+    for (var key in props.announcement) {
+      if (
+        props.announcement.hasOwnProperty(key) &&
+        !Array.isArray(props.announcement[key]) &&
+        typeof props.announcement[key] === "object"
+      ) {
+        if (props.announcement[key] !== null && key !== "id") {
+          details.push(getFieldDetails(key, props.announcement[key]));
+        }
+      }
+    }
+    return details;
+  };
+
+  const getPriceDetails = () => {
+    let additionalCosts = [];
+    for (var el in props.announcement.additionalCosts) {
+      const value =
+        props.announcement.additionalCosts[el]["costPrice"] === null
+          ? ""
+          : ": " + props.announcement.additionalCosts[el]["costPrice"];
+      const comma =
+        el == props.announcement.additionalCosts.length - 1 ? "" : ", ";
+      additionalCosts.push(
+        <div>
+          {props.announcement.additionalCosts[el]["costName"]}
+          {value}
+          {comma}
+        </div>
+      );
+    }
     return (
-      <Container>
-        <Row>{translation["lessor"]}</Row>
-        <Row>{lessorDetails}</Row>
+      <Container className="details-container">
+        <Row>
+          <h5>
+            {translation["baseCost"]}: <b>{props.announcement["baseCost"]}</b>
+          </h5>
+        </Row>
+        <Row>
+          <b>{translation["additionalCosts"]}: </b>
+          {additionalCosts}
+        </Row>
+      </Container>
+    );
+  };
+
+  const getSourceDetails = () => {
+    return (
+      <Container className="details-container">
+        <Row>
+          <span>
+            {translation["provider"]}:{" "}
+            <a href={props.announcement["url"]}>
+              {props.announcement["provider"]}
+            </a>{" "}
+          </span>{" "}
+          <span>
+            {translation["creationDate"]}: {props.announcement["creationDate"]}
+          </span>
+        </Row>
       </Container>
     );
   };
 
   const isDataLoaded = Object.keys(props.announcement).length > 0;
-  const lessorDetails = isDataLoaded ? getLessorDetails() : [];
+  const details = isDataLoaded ? getDetails() : [];
+  const priceDetails = isDataLoaded ? getPriceDetails() : [];
+  const sourceDetails = isDataLoaded ? getSourceDetails() : [];
 
   return (
     <Container>
-      {lessorDetails}
-      <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
-      </Row>
+      {priceDetails}
+      {details}
+      {sourceDetails}
     </Container>
   );
 };
--- krak-rent-frontend/src/pages/AnnouncementInfoPage.js
+++ krak-rent-frontend/src/pages/AnnouncementInfoPage.js
@@ -3,6 +3,7 @@ import { Card } from "react-bootstrap";
 import AnnouncementGallery from "../components/AnnouncementGallery";
 import AnnouncementDetails from "../components/AnnouncementDetails";
 import Axios from "axios";
+import translation from "../pl-translation.json";
 
 class AnnouncementInfoPage extends Component {
   constructor(props) {
@@ -31,6 +32,8 @@ class AnnouncementInfoPage extends Component {
     return this.state.announcement.images.split("\\");
   }
 
+  getDetailsState() {}
+
   render() {
     const isDataLoading = Object.keys(this.state.announcement).length === 0;
     const title = isDataLoading ? "Loading..." : this.state.announcement.title;
@@ -38,14 +41,25 @@ class AnnouncementInfoPage extends Component {
     const description = isDataLoading
       ? "Loading..."
       : this.state.announcement.description;
+    const detailsProps = isDataLoading
+      ? {}
+      : Object.keys(this.state.announcement)
+          .filter(key => key in translation)
+          .reduce((obj, key) => {
+            obj[key] = this.state.announcement[key];
+            return obj;
+          }, {});
+
     return (
       <div>
         <Card bg="light">
-          <Card.Header>{title}</Card.Header>
+          <Card.Header>
+            <h3>{title}</h3>
+          </Card.Header>
           <Card.Body>
             <AnnouncementGallery images={listOfImages} />
             <hr />
-            <AnnouncementDetails announcement={this.state.announcement} />
+            <AnnouncementDetails announcement={detailsProps} />
             <hr />
             {description}
           </Card.Body>
